11d76d30fc2b7e2cf71bbb4ec72ee81a7d530504
DatoKochla18 <shalvakochlamazashvili2@gmail.com>
2024-12-13 22:13:53 +0400

    Cleaned and added Fragments

---
+++ app/src/main/java/com/example/tbcexercises/EntryFragment.kt
@@ -0,0 +1,42 @@
+package com.example.tbcexercises
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tbcexercises.databinding.FragmentEntryBinding
+
+class EntryFragment : Fragment(R.layout.fragment_entry) {
+    private lateinit var binding: FragmentEntryBinding
+    private val loginFragment = LoginFragment()
+    private val registerFragment = RegisterFragment()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            FragmentEntryBinding.inflate(layoutInflater, container, false)
+        listeners()
+        return binding.root
+    }
+
+    private fun listeners() {
+        binding.btnLogin.setOnClickListener {
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, loginFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        binding.btnRegister.setOnClickListener {
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, registerFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+}
