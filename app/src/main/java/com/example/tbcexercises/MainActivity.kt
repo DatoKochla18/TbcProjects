06aa28e93e3ba8659b9d4d8213476fa0ac3f585c
DatoKochla18 <shalvakochlamazashvili2@gmail.com>
2024-12-29 17:59:17 +0400

    minor changes

--- app/src/main/java/com/example/tbcexercises/MainActivity.kt
+++ app/src/main/java/com/example/tbcexercises/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tbcexercises.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -18,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         supportFragmentManager.beginTransaction().run {
+            setReorderingAllowed(true)
             replace(R.id.frContainer, OrderListFragment())
             commit()
         }
