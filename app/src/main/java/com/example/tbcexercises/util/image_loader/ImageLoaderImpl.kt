247c0935ca3ac00d55084da7daf950bf198020bf
DatoKochla18 <shalvakochlamazashvili2@gmail.com>
2025-02-23 21:39:02 +0400

    fixed ui

--- app/src/main/java/com/example/tbcexercises/util/image_loader/ImageLoaderImpl.kt
+++ app/src/main/java/com/example/tbcexercises/util/image_loader/ImageLoaderImpl.kt
@@ -3,8 +3,9 @@ package com.example.tbcexercises.util.image_loader
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
-object ImageLoaderImpl:ImageLoader {
-    override fun loadImage(imageView: ImageView, url: String?,) {
+object ImageLoaderImpl : ImageLoader {
+
+    override fun loadImage(imageView: ImageView, url: String?) {
         Glide.with(imageView.context)
             .load(url)
             .into(imageView)
